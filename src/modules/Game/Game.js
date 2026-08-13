fb0913b6bb07b1d7070b0fcd650323ee53df90e8
Luis Gabriel <luis.gabo.rex@gmail.com>
2026-08-13 09:23:20 -0600

    feat: implement Game controller

---
+++ src/modules/Game/Game.js
@@ -0,0 +1,45 @@
+export default class Game {
+  constructor(player1, player2) {
+    this.attacker = player1;
+    this.defender = player2;
+  }
+
+  playTurn(xCoordinate, yCoordinate) {
+    let results = null;
+    if (arguments.length > 0) {
+      results = this.attacker.attack(
+        this.defender.getGameBoard(),
+        xCoordinate,
+        yCoordinate,
+      );
+    } else {
+      results = this.attacker.attack(this.defender.getGameBoard());
+    }
+
+    if (!this.checkWinner()) {
+      this.changeTurn();
+    }
+
+    return {
+      ...results,
+      winner: this.checkWinner(),
+    };
+  }
+
+  changeTurn() {
+    const formerAttacker = this.attacker;
+    this.attacker = this.defender;
+    this.defender = formerAttacker;
+  }
+
+  checkWinner() {
+    if (this.defender.getGameBoard().allShipsSunk()) {
+      return this.attacker;
+    }
+
+    return null;
+  }
+}
+
+//modificar gameboard y player para que retoner el resultado del ataque, de esta forma se puede saber
+//desde game, y poder retornar el resultado del turn, modificar los test
---
+++ src/modules/Game/Game.test.js
@@ -0,0 +1,174 @@
+import Game from "./Game";
+import Player from "../Player/Player";
+import Computer from "../Computer/Computer";
+
+describe("Game", () => {
+  test("starts with Player 1 as attacker and Player 2 as defender", () => {
+    const player1 = new Player("Player 1");
+    const player2 = new Player("Player 2");
+
+    const game = new Game(player1, player2);
+
+    expect(game.attacker).toBe(player1);
+    expect(game.defender).toBe(player2);
+  });
+
+  test("attacker attacks the defender at the given coordinates", () => {
+    const player1 = new Player("Player 1");
+    const player2 = new Player("Player 2");
+
+    const enemyBoard = player2.getGameBoard();
+    const receiveAttack = jest.spyOn(enemyBoard, "receiveAttack");
+
+    const game = new Game(player1, player2);
+
+    game.playTurn(3, 4);
+
+    expect(receiveAttack).toHaveBeenCalledWith(3, 4);
+  });
+
+  test("changes attacker and defender after a turn without a winner", () => {
+    const player1 = new Player("Player 1");
+    const player2 = new Player("Player 2");
+
+    const game = new Game(player1, player2);
+
+    game.playTurn(3, 4);
+
+    expect(game.attacker).toBe(player2);
+    expect(game.defender).toBe(player1);
+  });
+
+  test("does not change attacker and defender after a winning attack", () => {
+    const player1 = new Player("Player 1");
+    const player2 = new Player("Player 2");
+
+    player2.getGameBoard().placeShip(0, 0, "x", 1);
+
+    const game = new Game(player1, player2);
+
+    game.playTurn(0, 0);
+
+    expect(game.attacker).toBe(player1);
+    expect(game.defender).toBe(player2);
+  });
+
+  test("Computer can play a turn without receiving coordinates", () => {
+    const computer = new Computer("Computer");
+    const player = new Player("Player");
+
+    const enemyBoard = player.getGameBoard();
+    const receiveAttack = jest.spyOn(enemyBoard, "receiveAttack");
+
+    const game = new Game(computer, player);
+
+    game.playTurn();
+
+    expect(receiveAttack).toHaveBeenCalledTimes(1);
+  });
+
+  test("changes attacker and defender after Computer's turn", () => {
+    const computer = new Computer("Computer");
+    const player = new Player("Player");
+
+    const game = new Game(computer, player);
+
+    game.playTurn();
+
+    expect(game.attacker).toBe(player);
+    expect(game.defender).toBe(computer);
+  });
+
+  test("checkWinner returns the attacker when the defender has lost", () => {
+    const player1 = new Player("Player 1");
+    const player2 = new Player("Player 2");
+
+    player2.getGameBoard().placeShip(0, 0, "x", 1);
+
+    const game = new Game(player1, player2);
+
+    player1.attack(player2.getGameBoard(), 0, 0);
+
+    expect(game.checkWinner()).toBe(player1);
+  });
+
+  test("checkWinner returns null when the defender still has ships", () => {
+    const player1 = new Player("Player 1");
+    const player2 = new Player("Player 2");
+
+    player2.getGameBoard().placeShip(0, 0, "x", 2);
+
+    const game = new Game(player1, player2);
+
+    player1.attack(player2.getGameBoard(), 0, 0);
+
+    expect(game.checkWinner()).toBeNull();
+  });
+
+  test("playTurn returns the result of a missed attack", () => {
+    const player1 = new Player("Player 1");
+    const player2 = new Player("Player 2");
+
+    const game = new Game(player1, player2);
+
+    const result = game.playTurn(3, 4);
+
+    expect(result).toEqual({
+      winner: null,
+      attackResult: "miss",
+      sunkedShip: false,
+    });
+  });
+
+  test("playTurn returns hit when an attack hits a ship without sinking it", () => {
+    const player1 = new Player("Player 1");
+    const player2 = new Player("Player 2");
+
+    player2.getGameBoard().placeShip(0, 0, "x", 2);
+
+    const game = new Game(player1, player2);
+
+    const result = game.playTurn(0, 0);
+
+    expect(result).toEqual({
+      winner: null,
+      attackResult: "hit",
+      sunkedShip: false,
+    });
+  });
+
+  test("playTurn returns the winner and sunked ship when the attack wins the game", () => {
+    const player1 = new Player("Player 1");
+    const player2 = new Player("Player 2");
+
+    player2.getGameBoard().placeShip(0, 0, "x", 1);
+
+    const game = new Game(player1, player2);
+
+    const result = game.playTurn(0, 0);
+
+    expect(result).toEqual({
+      winner: player1,
+      attackResult: "hit",
+      sunkedShip: true,
+    });
+  });
+
+  test("playTurn returns sunkedShip true when a ship is sunk but the game continues", () => {
+    const player1 = new Player("Player 1");
+    const player2 = new Player("Player 2");
+
+    player2.getGameBoard().placeShip(0, 0, "x", 1);
+    player2.getGameBoard().placeShip(5, 5, "x", 2);
+
+    const game = new Game(player1, player2);
+
+    const result = game.playTurn(0, 0);
+
+    expect(result).toEqual({
+      winner: null,
+      attackResult: "hit",
+      sunkedShip: true,
+    });
+  });
+});
