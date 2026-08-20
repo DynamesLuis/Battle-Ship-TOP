6e0892eaa7eb569d03b73102e8f01944f3fca914
Luis Gabriel <luis.gabo.rex@gmail.com>
2026-08-20 13:47:15 -0600

    feat: add random dialogue selection to Character

--- src/modules/Character/Character.js
+++ src/modules/Character/Character.js
@@ -1,19 +1,29 @@
 export default class Character {
-    constructor(name, dialogues, img) {
-        this.name = name;
-        this.dialogues = dialogues;
-        this.img = img;
-    }
+  constructor(name, dialogues, img) {
+    this.name = name;
+    this.dialogues = dialogues;
+    this.img = img;
+  }
 
-    getName() {
-        return this.name;
-    }
+  getName() {
+    return this.name;
+  }
 
-    getImg() {
-        return this.img;
-    }
+  getImg() {
+    return this.img;
+  }
 
-    getDialogues() {
-        return this.dialogues;
-    }
+  getDialogues() {
+    return this.dialogues;
+  }
+
+  getRandomDialogue(action) {
+    const dialogues = this.dialogues[action];
+    const index = this.#getRandomIndex(dialogues);
+    return dialogues[index];
+  }
+
+  #getRandomIndex(array) {
+    return Math.floor(Math.random() * array.length);
+  }
 }
--- src/modules/Character/Character.test.js
+++ src/modules/Character/Character.test.js
@@ -33,3 +33,43 @@ describe.skip("Character", () => {
     expect(character.getDialogues()).toEqual(dialogues);
   });
 });
+
+describe.skip("getRandomDialogue", () => {
+  let character;
+  let dialogues;
+
+  beforeEach(() => {
+    dialogues = {
+      hit: ["Nice shot!", "Got you!"],
+      miss: ["You missed!", "Try again!"],
+      sunk: ["You sank my ship!", "That's one down!"],
+      win: ["We won!", "Victory!"],
+    };
+
+    character = new Character("Captain", dialogues, "captain.png");
+  });
+
+  test("returns a random dialogue from hit dialogues", () => {
+    const result = character.getRandomDialogue("hit");
+
+    expect(dialogues.hit).toContain(result);
+  });
+
+  test("returns a random dialogue from miss dialogues", () => {
+    const result = character.getRandomDialogue("miss");
+
+    expect(dialogues.miss).toContain(result);
+  });
+
+  test("returns a random dialogue from sunk dialogues", () => {
+    const result = character.getRandomDialogue("sunk");
+
+    expect(dialogues.sunk).toContain(result);
+  });
+
+  test("returns a random dialogue from win dialogues", () => {
+    const result = character.getRandomDialogue("win");
+
+    expect(dialogues.win).toContain(result);
+  });
+});
