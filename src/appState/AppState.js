28e015f9e350f712933a034c2dab833aecbcc5c1
Luis Gabriel <luis.gabo.rex@gmail.com>
2026-08-25 08:47:54 -0600

    feat: add AppState for application data

---
+++ src/appState/AppState.js
@@ -0,0 +1,49 @@
+export default class AppState {
+  constructor() {
+    this.name = null;
+    this.character1 = null;
+    this.character2 = null;
+    this.player1 = null;
+    this.player2 = null;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getCharacter1() {
+    return this.character1;
+  }
+
+  getCharacter2() {
+    return this.character2;
+  }
+
+  getPlayer1() {
+    return this.player1;
+  }
+
+  getPlayer2() {
+    return this.player2;
+  }
+
+  setName(name) {
+    this.name = name;
+  }
+
+  getCharacter1(character1) {
+    this.character1 = character1;
+  }
+
+  getCharacter2(character2) {
+    this.character2 = character2;
+  }
+
+  getPlayer1(player1) {
+    this.player1 = player1;
+  }
+
+  getPlayer2(player2) {
+    this.player2 = player2;
+  }
+}
