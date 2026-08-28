2bc793809c5f8c91898fe5a8cceae43ff5e4f7c0
Luis Gabriel <luis.gabo.rex@gmail.com>
2026-08-28 15:09:37 -0600

    feat: add ship data

---
+++ src/gameData/shipsData.js
@@ -0,0 +1,29 @@
+const shipsData = [
+  {
+    id: "1",
+    name: "Flagship",
+    length: 5,
+  },
+  {
+    id: "2",
+    name: "Battleship",
+    length: 4,
+  },
+  {
+    id: "3",
+    name: "Destroyer",
+    length: 3,
+  },
+  {
+    id: "4",
+    name: "Gunship",
+    length: 3,
+  },
+  {
+    id: "5",
+    name: "Scout Ship",
+    length: 2,
+  },
+];
+
+export default shipsData;
