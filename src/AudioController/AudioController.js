49d0b3193178e11ba69cddd95ef57cf6b51780d2
Luis Gabriel <luis.gabo.rex@gmail.com>
2026-09-11 11:10:14 -0600

    feat: add menu and battle music controls

--- src/AudioController/AudioController.js
+++ src/AudioController/AudioController.js
@@ -3,11 +3,19 @@ import hit from "../assets/audios/hit.mp3";
 import canon from "../assets/audios/canon.mp3";
 import sunk from "../assets/audios/sunk.mp3";
 import victory from "../assets/audios/victory.mp3";
+import menu1 from "../assets/audios/backgroundmusicmaster-fantasy-craft-loop-431346.mp3";
+import menu2 from "../assets/audios/blendertimer-medieval-kingdoms-598387.mp3";
+import battle1 from "../assets/audios/hitslab-battle-fighting-warrior-drums-372078.mp3";
+import battle2 from "../assets/audios/thefealdoproject-the-battle-of-coalitions-115596.mp3";
 import delay from "../helpers/delay";
 
 export default class AudioController {
   constructor() {
     this.delay = 500;
+    this.currentMusic = null;
+    this.typeMusic = null;
+    this.menuMusic = [menu1, menu2];
+    this.battleMusic = [battle1, battle2];
   }
 
   playSound(sound) {
@@ -36,5 +44,44 @@ export default class AudioController {
 
   playVictory() {
     this.playSound(victory);
+  }
+
+  playMenuMusic() {
+    if (this.typeMusic === "menu") return;
+    const music = new Audio(this.#getRandomMusic("menu"));
+    this.stopMusic();
+    this.currentMusic = music;
+    music.play();
+    music.loop = true;
+    this.typeMusic = "menu";
+  }
+
+  playBattleMusic() {
+    if (this.typeMusic === "battle") return;
+    const music = new Audio(this.#getRandomMusic("battle"));
+    this.stopMusic();
+    this.currentMusic = music;
+    music.play();
+    music.loop = true;
+    this.typeMusic = "battle";
+  }
+
+  stopMusic() {
+    if (this.currentMusic) {
+      this.currentMusic.loop = false;
+      this.currentMusic.pause();
+      this.currentMusic.currentTime = 0;
+    }
+  }
+
+  #getRandomMusic(type) {
+    let music = null;
+    if (type === "menu") {
+      music = this.menuMusic[Math.floor(Math.random() * this.menuMusic.length)];
+    } else {
+      music =
+        this.battleMusic[Math.floor(Math.random() * this.battleMusic.length)];
+    }
+    return music;
   }
 }
--- src/AudioController/AudioController.test.js
+++ src/AudioController/AudioController.test.js
@@ -8,55 +8,141 @@ import delay from "../helpers/delay";
 
 jest.mock("../helpers/delay", () => jest.fn(() => Promise.resolve()));
 
-global.Audio = jest.fn(() => ({
-  play: jest.fn(),
-}));
+global.Audio = jest.fn(function () {
+  this.play = jest.fn();
+  this.pause = jest.fn();
+  this.loop = false;
+});
 
-describe.skip("AudioController", () => {
-  test("can be created", () => {
-    const audioController = new AudioController();
-    expect(audioController).toBeDefined();
+describe("AudioController", () => {
+  let audioController;
+  beforeEach(() => {
+    audioController = new AudioController();
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
-  test("can play a sound", () => {
-    const audioController = new AudioController();
-    const sound = "sound.mp3";
-    audioController.playSound(sound);
-    expect(global.Audio).toHaveBeenCalledWith(sound);
+  describe.skip("AudioController - effects", () => {
+    test("can be created", () => {
+      const audioController = new AudioController();
+      expect(audioController).toBeDefined();
+    });
+
+    test("can play a sound", () => {
+      const audioController = new AudioController();
+      const sound = "sound.mp3";
+      audioController.playSound(sound);
+      expect(global.Audio).toHaveBeenCalledWith(sound);
+    });
+
+    test("playShot() calls playSound() with the shot sound", () => {
+      const audioController = new AudioController();
+      jest.spyOn(audioController, "playSound");
+      audioController.playShot();
+      expect(audioController.playSound).toHaveBeenCalledWith(canon);
+    });
+
+    test("playHit() calls playSound() with the hit sound", async () => {
+      const audioController = new AudioController();
+      jest.spyOn(audioController, "playSound");
+      await audioController.playHit();
+      expect(audioController.playSound).toHaveBeenCalledWith(hit);
+    });
+
+    test("playMiss() calls playSound() with the miss sound", async () => {
+      const audioController = new AudioController();
+      jest.spyOn(audioController, "playSound");
+      await audioController.playMiss();
+      expect(audioController.playSound).toHaveBeenCalledWith(miss);
+    });
+
+    test("playSunk() calls playSound() with the sunk sound", async () => {
+      const audioController = new AudioController();
+      jest.spyOn(audioController, "playSound");
+      await audioController.playSunk();
+      expect(audioController.playSound).toHaveBeenCalledWith(sunk);
+    });
+
+    test("playVictory() calls playSound() with the victory sound", () => {
+      const audioController = new AudioController();
+      jest.spyOn(audioController, "playSound");
+      audioController.playVictory();
+      expect(audioController.playSound).toHaveBeenCalledWith(victory);
+    });
   });
 
-  test("playShot() calls playSound() with the shot sound", () => {
-    const audioController = new AudioController();
-    jest.spyOn(audioController, "playSound");
-    audioController.playShot();
-    expect(audioController.playSound).toHaveBeenCalledWith(canon);
-  });
+  describe("AudioController - Music", () => {
+    let audioController;
 
-  test("playHit() calls playSound() with the hit sound", async () => {
-    const audioController = new AudioController();
-    jest.spyOn(audioController, "playSound");
-    await audioController.playHit();
-    expect(audioController.playSound).toHaveBeenCalledWith(hit);
-  });
+    beforeEach(() => {
+      audioController = new AudioController();
+    });
 
-  test("playMiss() calls playSound() with the miss sound", async () => {
-    const audioController = new AudioController();
-    jest.spyOn(audioController, "playSound");
-    await audioController.playMiss();
-    expect(audioController.playSound).toHaveBeenCalledWith(miss);
-  });
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
 
-  test("playSunk() calls playSound() with the sunk sound", async () => {
-    const audioController = new AudioController();
-    jest.spyOn(audioController, "playSound");
-    await audioController.playSunk();
-    expect(audioController.playSound).toHaveBeenCalledWith(sunk);
-  });
+    test("can start menu music", () => {
+      audioController.playMenuMusic();
+      expect(Audio).toHaveBeenCalledTimes(1);
+      expect(Audio.mock.instances[0].play).toHaveBeenCalledTimes(1);
+    });
 
-  test("playVictory() calls playSound() with the victory sound", () => {
-    const audioController = new AudioController();
-    jest.spyOn(audioController, "playSound");
-    audioController.playVictory();
-    expect(audioController.playSound).toHaveBeenCalledWith(victory);
+    test("plays one of the available menu songs", () => {
+      audioController.playMenuMusic();
+      const playedMusic = Audio.mock.calls[0][0];
+      expect(audioController.menuMusic).toContain(playedMusic);
+    });
+
+    test("menu music is configured to loop", () => {
+      audioController.playMenuMusic();
+      const music = Audio.mock.instances[0];
+      expect(music.loop).toBe(true);
+    });
+
+    test("does not restart menu music if menu music is already playing", () => {
+      audioController.playMenuMusic();
+      audioController.playMenuMusic();
+      expect(Audio).toHaveBeenCalledTimes(1);
+      expect(Audio.mock.instances[0].play).toHaveBeenCalledTimes(1);
+    });
+
+    test("playBattleMusic stops the current music and starts battle music", () => {
+      audioController.playMenuMusic();
+      const menuMusic = Audio.mock.instances[0];
+      audioController.playBattleMusic();
+      expect(menuMusic.pause).toHaveBeenCalledTimes(1);
+      expect(Audio).toHaveBeenCalledTimes(2);
+      expect(Audio.mock.instances[1].play).toHaveBeenCalledTimes(1);
+    });
+
+    test("plays one of the available battle songs", () => {
+      audioController.playBattleMusic();
+      const playedMusic = Audio.mock.calls[0][0];
+      expect(audioController.battleMusic).toContain(playedMusic);
+    });
+
+    test("battle music is configured to loop", () => {
+      audioController.playBattleMusic();
+      const music = Audio.mock.instances[0];
+      expect(music.loop).toBe(true);
+    });
+
+    test("stopMusic stops the current music", () => {
+      audioController.playMenuMusic();
+      const music = Audio.mock.instances[0];
+      audioController.stopMusic();
+      expect(music.pause).toHaveBeenCalledTimes(1);
+    });
+
+    test("playMenuMusic starts menu music after battle music", () => {
+      audioController.playBattleMusic();
+      const battleMusic = Audio.mock.instances[0];
+      audioController.playMenuMusic();
+      expect(battleMusic.pause).toHaveBeenCalledTimes(1);
+      expect(Audio).toHaveBeenCalledTimes(2);
+      expect(Audio.mock.instances[1].play).toHaveBeenCalledTimes(1);
+    });
   });
 });
