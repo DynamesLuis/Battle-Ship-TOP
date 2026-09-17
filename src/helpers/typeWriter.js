d46df4046eab282a519aab2be678cbf2928cbc7d
Luis Gabriel <luis.gabo.rex@gmail.com>
2026-09-17 11:49:36 -0600

    feat: wrap battle report messages in quotes

--- src/helpers/typeWriter.js
+++ src/helpers/typeWriter.js
@@ -1,13 +1,16 @@
 import delay from "./delay";
 
 async function typeWriter(element, text, speed = 35) {
-  element.textContent = "";
+  element.textContent = '"';
   element.classList.add("typing");
 
+  element.textContent
   for (const character of text) {
     element.textContent += character;
     await delay(speed);
   }
+
+  element.textContent += '"';
 
   element.classList.remove("typing");
 }
